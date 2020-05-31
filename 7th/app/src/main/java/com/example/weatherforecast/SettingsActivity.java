4ce6838858109d0913d39b1294035435d4c15b22
최수용 <dpfprtus@gmail.com>
2020-05-31 23:43:21 +0900

    Create SettingsActivity.java

---
+++ 7th/app/src/main/java/com/example/weatherforecast/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.weatherforecast;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+    }
+}
